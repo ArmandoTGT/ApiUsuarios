adabb771584ce9164f43c6ceab8bff570ca6f8ac
Drayton80 <drayton80@hotmail.com>
2019-03-14 17:55:08 -0300

    Atualizações na Classe User

--- business/control/AddUsuario.py
+++ business/control/AddUsuario.py
@@ -1,7 +1,16 @@
-
+from user import User
 
 emailUsuario = input()
 nomeUsuario = input()
 senhaUsuario = input()
 
-usuario = Usuario(nomeUsuario, emailUsuario, senhaUsuario)
+try:
+	usuario = User(nomeUsuario, emailUsuario, senhaUsuario)
+except outOfBoundsUserNameException:
+	print('except')
+except incompatibleCharacterUserNameException:
+	print('except')
+except passwordMissingCharactersException:
+	print('except')
+except outOfBoundsPasswordException:
+	print('except')
--- business/control/user.py
+++ business/control/user.py
@@ -1,17 +1,31 @@
-class User:
+import re
 
+class User:
 	def __init__(self, user_name, email, password):
 		self.user_name = user_name
 		self.email = email
 		self.password = password
 
 	def set_user_name(self, user_name):
+		if len(user_name) == 0 or len(user_name) > 15:
+			raise outOfBoundsUserNameException('User name is out of the specific range of 0~15 charactes')
+
+		if bool(re.match('^(?=.*[0-9])', user_name)):
+			raise incompatibleCharacterUserNameException('Contains a incompatible character')
+
 		self.user_name = user_name
 
 	def set_email(self, email):
 		self.email = email
 
 	def set_password(self, password):
+		if (not bool(re.match('^(?=.*[0-9])', password)) and 
+            not bool(re.match('^(?=.*[a-zA-Z])', password))):
+			raise passwordMissingCharactersException('Missing a specif type of characters')
+
+		if len(password) < 6 or 16 < len(password):
+			raise outOfBoundsPasswordException('Password name is out of the specific range of 0~16 charactes')
+
 		self.password = password
 
 	def get_user_name(self):
