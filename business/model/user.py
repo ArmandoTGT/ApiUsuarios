eda0aa1fb9bc97e5e628fefe03d8c53cdb3c76ea
ArmandoTGT <armando_tgt@hotmail.com>
2019-03-18 14:04:38 -0300

    Reestruturação

---
+++ business/model/user.py
@@ -0,0 +1,28 @@
+import re
+
+class User:
+	def __init__(self, user_name, email, password):
+		self.user_name = user_name
+		self.email = email
+		self.password = password
+
+	def set_user_name(self, user_name):
+		self.user_name = user_name
+
+	def set_email(self, email):
+		self.email = email
+
+	def set_password(self, password):
+		self.password = password
+
+	def get_user_name(self):
+		return self.user_name
+
+	def get_email(self):
+		return self.email
+
+	def get_password(self):
+		return self.password
+
+
+
