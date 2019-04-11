ea4e6dca042bbae469b6ed9c0f6fbd0cc74fa889
ArmandoTGT <armando_tgt@hotmail.com>
2019-04-11 16:57:58 -0300

    PersistenciaXML

---
+++ infra/usuariosLogadosXML.py
@@ -0,0 +1,58 @@
+import sys
+from django.core import serializers
+import pickle
+import collections
+from datetime import datetime
+
+from business.model.User import User
+from infra import DBControl
+
+def loadLoggedUsers():
+    try:
+        with open('loggedUsers.pck', 'rb') as arq:
+            dict_log = serializers.deserialize('xml', pickle.load(arq)) 
+            arq.close()
+    except Exception:
+        return collections.OrderedDict()
+
+    return dict_log
+
+def saveLoggedUsers(email):
+
+    dict_log = loadLoggedUsers()
+
+    dbCrontol = DBControl()
+
+    dict_log[email] = dbCrontol.getUser(email)
+
+    allInXML = serializers.serialize('xml', dict_log)
+
+    with open('loggedUsers.pck', 'wb') as arq:
+        pickle.dump(allInXML, arq, pickle.HIGHEST_PROTOCOL)
+        arq.close()
+
+    printLoggedUsers()
+
+def printLoggedUsers():
+    dict_log = loadLoggedUsers()
+
+    list = []
+    for user in dict_log:
+        data = dict_log[user].getNascimento()
+        list.append((data, dict_log[user].getNome()))
+
+    list.sort(key=lambda date: datetime.strptime(date[0], "%d/%m/%Y"))
+
+    print('\nLista Ordenada:', list)
+
+def deleteLoggedUsers(email):
+
+    dict_log = loadLoggedUsers()
+    del dict_log[email]
+
+    allInXML = serializers.serialize('xml', dict_log)
+
+    with open('loggedUsers.pck', 'wb') as arq:
+        pickle.dump(allInXML, arq, pickle.HIGHEST_PROTOCOL)
+        arq.close()
+    printLoggedUsers()
